83eca9d933d3ce71fde0a504fdff29f434e8513d
lszweda <lszweda@psi.pl>
2022-12-13 18:58:50 +0100

    adding onlyRegistered

--- Blockchain/contracts/Marketplace.sol
+++ Blockchain/contracts/Marketplace.sol
@@ -18,16 +18,23 @@ contract Marketplace is Ownable {
     
     mapping (address => CollectionDO[]) public userToCollections;
     mapping (uint => uint) public idToNumberLeft;
+    mapping( address => bool ) public collectionAvailability;
     
 
     event CollectionCreated( string collectionName, string collectionSymbol, address collectionAddress, address user );
     event NFTminted( uint _tokenId, string collectionName, string collectionSymbol, address collectionAddress, address userAddress );
 
 
+    modifier onlyRegisteredCollection( Collection collection ) {
+        require( collectionAvailability[ address( collection ) ], "Address is not a Marketplace Collection." );
+        _;
+    }
+
     function createCollection(string memory _name, string memory _symbol) public {
         NFTcollection nfts = new NFTcollection(_name, _symbol);
         CollectionDO memory newCollection = CollectionDO(_name, _symbol, msg.sender, address(nfts));
         userToCollections[msg.sender].push(newCollection);
+        collectionAvailability[msg.sender] = true;
 
         emit CollectionCreated( _name, _symbol, msg.sender ); 
     } 
@@ -37,13 +44,6 @@ contract Marketplace is Ownable {
         emit NFTminted( tokenId, collection.name(), collection.symbol(), address(_collection), msg.sender); 
     }
 
-    function getAllCollections() public {
-        
-    }
-
-    function getUserCollections() public {
-
-    }
 
     function createListing(uint _nftId) public {
 

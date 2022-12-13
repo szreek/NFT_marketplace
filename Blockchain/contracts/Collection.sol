f3b26e08f716dbc7863e821f107a5120aa55b3d4
lszweda <lszweda@psi.pl>
2022-12-13 18:44:43 +0100

    implementing creteCollection and mint

--- Blockchain/contracts/Collection.sol
+++
@@ -1,8 +0,0 @@
-
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-pragma abicoder v2;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract Collection is ERC721  {
-}
--- Blockchain/contracts/Marketplace.sol
+++ Blockchain/contracts/Marketplace.sol
@@ -3,28 +3,45 @@
 pragma solidity ^0.8.0;
 pragma abicoder v2;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./NFTcollection.sol";
 
 contract Marketplace is Ownable {
 
     
+    struct CollectionDO {
+        string name;
+        string symbol;
+        address creatorAddress;
+        address nftsAddress;
+    }
     
     
+    mapping (address => CollectionDO[]) public userToCollections;
+    mapping (uint => uint) public idToNumberLeft;
     
-    
-    
-    function createCollection() public {
-        
+
+    event CollectionCreated( string collectionName, string collectionSymbol, address collectionAddress, address user );
+    event NFTminted( uint _tokenId, string collectionName, string collectionSymbol, address collectionAddress, address userAddress );
+
+
+    function createCollection(string memory _name, string memory _symbol) public {
+        NFTcollection nfts = new NFTcollection(_name, _symbol);
+        CollectionDO memory newCollection = CollectionDO(_name, _symbol, msg.sender, address(nfts));
+        userToCollections[msg.sender].push(newCollection);
+
+        emit CollectionCreated( _name, _symbol, msg.sender ); 
     } 
 
-    function mint() public {
-        
+    function mint(NFTcollection _collection, string memory _uri) public {
+        uint tokenId = _collection.safeMint(msg.sender, _uri);
+        emit NFTminted( tokenId, collection.name(), collection.symbol(), address(_collection), msg.sender); 
     }
 
     function getAllCollections() public {
-
+        
     }
 
-    function getUserCollection() public {
+    function getUserCollections() public {
 
     }
 
@@ -41,7 +58,7 @@ contract Marketplace is Ownable {
     }
 
     function getAllOffersForUser() public {
-        
+
     }
 
 }

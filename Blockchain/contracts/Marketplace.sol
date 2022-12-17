cf046320635ee91839fcb016a21e29f63884c6e1
lszweda <lszweda@psi.pl>
2022-12-17 14:46:00 +0100

    adding payment functions

--- Blockchain/contracts/Marketplace.sol
+++ Blockchain/contracts/Marketplace.sol
@@ -18,10 +18,13 @@ contract Marketplace is Ownable {
     mapping (address => CollectionDO[]) public userToCollections;
     mapping (uint => uint) public idToNumberLeft;
     mapping( address => bool ) public collectionAvailability;
+    mapping( address => uint256 ) public userToFunds;
     
 
     event CollectionCreated( string collectionName, string collectionSymbol, address collectionAddress, address user );
     event NFTminted( uint _tokenId, string collectionName, string collectionSymbol, address collectionAddress, address userAddress );
+    event FundsDeposited( uint funds, address userAddress );
+    event FundsReturned( uint funds, address userAddress );
 
 
     modifier onlyRegisteredCollection( NFTcollection _collection ) {
@@ -29,13 +32,33 @@ contract Marketplace is Ownable {
         _;
     }
 
+    receive() external payable {
+        userToFunds[ msg.sender ] += msg.value;
+        emit FundsDeposited( msg.value, msg.sender );
+    }
+    
+    fallback() external payable {
+        userToFunds[ msg.sender ] += msg.value;
+        emit FundsDeposited( msg.value, msg.sender );
+    }
+
+    function returnFunds( uint256 amount ) external {
+        require( userToFunds[ msg.sender ] >= amount, "Not enough funds." );
+        userToFunds[ msg.sender ] = userToFunds[ msg.sender ] - amount;
+        ( bool sent, ) = msg.sender.call{ value: amount }("");
+        require( sent, "Could not send Ether. Please try again :)" );
+
+        emit FundsReturned( amount,  msg.sender );
+    }
+
     function createCollection(string memory _name, string memory _symbol) public {
         NFTcollection nfts = new NFTcollection(_name, _symbol);
-        CollectionDO memory newCollection = CollectionDO(_name, _symbol, msg.sender, address(nfts));
+        address nftColAddress = address(nfts);
+        CollectionDO memory newCollection = CollectionDO(_name, _symbol, msg.sender, nftColAddress);
         userToCollections[msg.sender].push(newCollection);
-        collectionAvailability[address(nfts)] = true;
+        collectionAvailability[nftColAddress] = true;
 
-        emit CollectionCreated( _name, _symbol, address(nfts), msg.sender ); 
+        emit CollectionCreated( _name, _symbol, nftColAddress, msg.sender ); 
     } 
 
     function mint(NFTcollection _collection, string memory _uri) onlyRegisteredCollection(_collection) public {
@@ -44,16 +67,25 @@ contract Marketplace is Ownable {
     }
 
 
-    function createListing(uint _nftId) public {
+    function createListing(uint _tokenId, uint _price, NFTcollection _collection ) onlyRegisteredCollection(_collection) public {
+
+    }
+
+
+    function removeListing(uint _tokenId, uint _price, NFTcollection _collection ) onlyRegisteredCollection(_collection) public {
+
+    }
+
+    
+    function createOffer(uint _tokenId, uint _price, NFTcollection _collection) public {
+
+    }
+
+    function removeOffer(uint _tokenId, uint _price, NFTcollection _collection) public {
 
     }
 
     function buy(uint _nftId) public {
 
     }
-
-    function makeOffer(uint _nftId) public {
-
-    }
-
 }
